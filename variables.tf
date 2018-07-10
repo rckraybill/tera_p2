03bcba0c31312a26b975e1d1bd656784175ca4bf
Ross Kraybill <rkrayb2@mlrw20096053.ch.discoverfinancial.com>
2018-07-10 15:41:08 -0500

    new repo

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  default = "us-west-1"
+  description = "AWS Region"
+}
+
+variable "aws_ssh_admin_key_file {}
+
