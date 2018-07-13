b9ec9eccb0bcf84788467cd22f7bb6877f3caf41
Ross Kraybill <rckraybill@gmail.com>
2018-07-13 08:53:35 -0500

    added s3

--- instances.tf
+++ instances.tf
@@ -1,15 +1,30 @@
-resource "aws_instance" "dev" {
-	ami = "${var.ami["${var.aws_region}"]}"
-	instance_type	= "${var.aws_instance_type}"
-	key_name	= "${aws_key_pair.admin_key.key_name}"
-	security_groups	= ["${aws_security_group.base_security_group.name}"]
-	associate_public_ip_address = true
-	count		= 1
+#resource "aws_instance" "dev" {
+#	ami = "${var.ami["${var.aws_region}"]}"
+#	instance_type	= "${var.aws_instance_type}"
+#	key_name	= "${aws_key_pair.admin_key.key_name}"
+#	security_groups	= ["${aws_security_group.base_security_group.name}"]
+#	associate_public_ip_address = true
+#	count		= 1
+#
+#	tags {
+#		Name = "Ubuntu launched by terraform"
+#	}
+#}
+
+resource "aws_s3_bucket" "iac_book" {
+	bucket = "iac-book"
+	acl = "public-read"
 
 	tags {
-		Name = "Ubuntu launched by terraform"
+		Name = "IAC Book Bucket in ${var.aws_region}"
 	}
 }
 
-
-
+resource "aws_s3_bucket_object" "index" { 
+	bucket = "${aws_s3_bucket.iac_book.bucket}"
+	key = "index.html"
+	content = "<h1>Hello Infrastructure-as-Code Cookbook!</h1>"
+	content_type = "text/html"
+	acl = "public-read"
+	#source = "index.html"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "login" {
-	value = "ssh ubuntu@${aws_instance.dev.public_ip} -i ${var.aws_ssh_admin_key_file}"
-}
+#output "login" {
+#	value = "ssh ubuntu@${aws_instance.dev.public_ip} -i ${var.aws_ssh_admin_key_file}"
+#}
+#
+#output "security_groups" {
+#	value = "${formatlist("%v", aws_instance.dev.security_groups)}"
+#}
+#
+#output "instance_information" {
+#  value = "${formatlist("instance: %v public: %v private: %v", aws_instance.dev.*.id, aws_instance.dev.*.public_ip, aws_instance.dev.*.private_ip)}"
+#}
 
-output "security_groups" {
-	value = "${formatlist("%v", aws_instance.dev.security_groups)}"
-}
-
-output "instance_information" {
-  value = "${formatlist("instance: %v public: %v private: %v", aws_instance.dev.*.id, aws_instance.dev.*.public_ip, aws_instance.dev.*.private_ip)}"
+output "S3" {
+  value = "http://s3-${aws_s3_bucket.iac_book.region}.amazonaws.com/${aws_s3_bucket.iac_book.id}/${aws_s3_bucket_object.index.key}"
 }
